d6bfe180d133c88075200dd0c9f33c91511c198c
Justin Blake <justin@blaix.com>
2025-09-24 21:04:54 -0400

    Move tests back into their own application
    so test packages aren't in the main gren package dependencies.

--- devenv.nix
+++ devenv.nix
@@ -6,25 +6,24 @@
   packages = [ 
     pkgs.entr
     pkgs.fd
-    pkgs.gren
     pkgs.nodejs_22
     pkgs.sqlite
   ];
 
   processes = {
-    test-db.exec = "npx --yes ws4sql --bind-host localhost --quick-db ./test-dbs/test.db";
-    db-with-auth.exec = "npx --yes ws4sql --bind-host localhost --port 12322 --db ./test-dbs/test-with-auth.yaml";
+    test-db.exec = "npx --yes ws4sql --bind-host localhost --quick-db ./tests/db/test.db";
+    db-with-auth.exec = "npx --yes ws4sql --bind-host localhost --port 12322 --db ./tests/test-with-auth.yaml";
   };
 
   tasks = {
     "test:resetDbs" = { 
       before = [ "devenv:enterTest" ];
       exec = ''
-        rm -f test-dbs/*.db && \
-        rm -f test-dbs/*.log && \
-        touch test-dbs/test.log && \
-        sqlite3 test-dbs/test-with-auth.db "create table auth (user text, password text)" && \
-        sqlite3 test-dbs/test-with-auth.db "insert into auth (user, password) values (\"myuser\", \"mypass\")"
+        mkdir -p tests/db && \
+        rm -f tests/db/* && \
+        touch tests/db/test.log && \
+        sqlite3 tests/db/test-with-auth.db "create table auth (user text, password text)" && \
+        sqlite3 tests/db/test-with-auth.db "insert into auth (user, password) values (\"myuser\", \"mypass\")"
       '';
     };
   };
@@ -32,6 +31,6 @@
   enterTest = ''
     wait_for_port 12321 && \
     wait_for_port 12322 && \
-    fd ".*\.gren" | entr -c -s 'gren make && gren run Tests'
+    fd ".*\.(gren|js)" | entr -c -s 'gren make && cd tests && gren run Main'
   '';
 }
